0a85564d36126431fea0db03bb70b705b1c9c128
Dekyi Tenzin <dekyitenzin@dekyis-air.myfiosgateway.com>
2023-09-19 13:15:16 -0400

    added board

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,16 @@
+import './home.css'
 const React = require('react')
+
+
+function Board() {
+    return <div className="rectangle">
+    </div> }
 
 function Home () {
     return(
             <main>
                 <h1>Home Page</h1>
+                <Board></Board>
             </main>
 
     )
