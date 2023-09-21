be3624f4d9da1c5963608354ca57e919b3eec263
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-09-21 18:41:53 -0400

    pushing latest api

--- frontend/src/components/places/More.jsx
+++ frontend/src/components/places/More.jsx
@@ -1,14 +1,11 @@
-const React = require('react')
-const Def = require('../NavBar.jsx')
 
 function More () {
-    return(
-        <>
+     return(
+       <>
         <h1>More Jobs</h1>
         
-        </>
-    )
-}
-
+        </>    
+		)
+ }
 
 export default More;
