e05456c58011442d4cd533035c6ee2cb02672289
Dekyi Tenzin <dekyitenzin@Dekyis-Air.lan>
2023-09-21 21:13:40 -0400

    presented work

--- frontend/src/components/places/More.jsx
+++ frontend/src/components/places/More.jsx
@@ -1,9 +1,36 @@
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function More () {
+    const [jobs,setJobs] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const response = await fetch ('http://localhost:5001/jobs')
+            const resData = await response.json()
+            setJobs(resData)
+
+        }
+        fetchData()
+    },[])
+
+    let jobsFormatted = jobs.map((job) => {
+        return(
+            <div key={job._id}>
+                <h1> 
+                {job.business} </h1>
+                <a href="#" onClick={() => navigate.push(`/jobs/${job.jobId}`)} >
+						{job.name}
+					</a>
+            </div>
+        )
+    })
+        console.log(jobs)
      return(
        <>
         <h1>More Jobs</h1>
-        
+        {jobsFormatted}
         </>    
 		)
  }
